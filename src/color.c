80116746f9746bbfe1f358af4ecf805e756b3588
Austin Dunn <awd123ps@gmail.com>
2015-10-03 10:18:24 -0500

    Fixed bugs.

--- src/color.c
+++ src/color.c
@@ -2,24 +2,29 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  int i;
+  int i, n;
 
   if (sizeof(argv[1]) == 0) {
     printf("Whoops! Wrong number of arguments! :( Exiting...\n");
+
     return 1;
   }
-  
-  for (i = 0; i < strlen(argv[1]); i++) {
+
+  for (i = 0, n = 0; i < strlen(argv[1]); i++, n++) {
     if (i == (strlen(argv[1]) - 1)) {
       printf("\e[%d;3%dm%c\e[0m\n",
-	     ((i < 8) ? 0 : 1),
-	     ((i < 8) ? i : (i - 7)),
+	     ((n < 8) ? 0 : 1),
+	     ((n < 8) ? n : (n - 7)),
 	     argv[1][i]);
     } else {
       printf("\e[%d;3%dm%c",
-	     ((i < 8) ? 0 : 1),
-	     ((i < 8) ? i : (i - 7)),
+	     ((n < 8) ? 0 : 1),
+	     ((n < 8) ? n : (n - 7)),
 	     argv[1][i]);
+    }
+
+    if (n == 14) {
+      n -= 14;
     }
   }
 
