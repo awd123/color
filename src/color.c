ec4ed663cb28b5ca07debf9da6fb6a39e6ffcb43
Austin Dunn <awd123ps@gmail.com>
2015-10-07 18:36:21 -0500

    Moved colorize function to color.h.

--- src/color.c
+++ src/color.c
@@ -1,29 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <regex.h>
-
-void
-colorize (char string[])
-{
-  int i, n;
-
-  for (i = 0, n = 0; i < strlen(string); i++, n++)
-    {
-      if (i == (strlen(string) - 1))
-        printf("\e[%d;3%dm%c\e[0m\n",
-	       ((n < 8) ? 0 : 1),
-	       ((n < 8) ? n : (n - 7)),
-	       string[i]);
-      else
-        printf("\e[%d;3%dm%c",
-	       ((n < 8) ? 0 : 1),
-	       ((n < 8) ? n : (n - 7)),
-	       string[i]);
-
-      if (n == 14)
-	n -= 14;
-    }
-}
+#include <Python.h>
+#include "color.h"
 
 int
 main(int argc, char *argv[])
@@ -37,7 +13,7 @@ main(int argc, char *argv[])
       printf("Entering interactive mode... (type exit to exit)\n");
       while (1)
 	{
-          printf("color>");
+          printf("color> ");
           fgets(str, sizeof(str), stdin);
             if (str == "exit")
 	      break;
