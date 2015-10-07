55428aaf4f9616a4c3a1661405f82f8e96351a13
Austin Dunn <awd123ps@gmail.com>
2015-10-07 18:36:47 -0500

    Created color.h.

---
+++ src/color.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void
+colorize (char string[])
+{
+  int i, n;
+
+  for (i = 0, n = 0; i < strlen(string); i++, n++)
+    {
+      if (i == (strlen(string) - 1))
+        printf("\e[%d;3%dm%c\e[0m\n",
+	       ((n < 8) ? 0 : 1),
+	       ((n < 8) ? n : (n - 7)),
+	       string[i]);
+      else
+        printf("\e[%d;3%dm%c",
+	       ((n < 8) ? 0 : 1),
+	       ((n < 8) ? n : (n - 7)),
+	       string[i]);
+
+      if (n == 14)
+	n -= 14;
+    }
+}
