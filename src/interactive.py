ccfaa9563e2b760e79a36d81f9de5efffaf4ebd7
Austin Dunn <awd123ps@gmail.com>
2015-10-07 20:32:02 -0500

    Finished interactive mode.

--- src/interactive.py
+++ src/interactive.py
@@ -1,6 +1,19 @@
+#!/usr/bin/python
+
 import sys
 
-def interactive:
+def py_colorize(string):
+    n = 0
+    for c in string:
+        colored_string += ("\e["
+                        + (0 if n < 8 else 1)
+                        + ";3"
+                        + (n if n < 8 else (n - 7))
+                        + "m"
+                        + c)
+    return colored_string
+
+def interactive():
     while True:
         in_string = raw_input("color> ")
         if in_string == q:
