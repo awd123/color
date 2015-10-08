695d65a3796c36f023928a9000360c815833a4d8
Austin Dunn <awd123ps@gmail.com>
2015-10-07 20:31:11 -0500

    Fixed Python code.

--- src/color.c
+++ src/color.c
@@ -4,8 +4,7 @@
 int
 main (int argc, char *argv[])
 {
-  PyObject *pName, *pModule, *pDict, *pFunc;
-  PyObject *pArgs, *pValue;
+  PyObject *pName, *pModule, *pFunc;
   int i;
   
   char str[1000];
@@ -15,7 +14,7 @@ main (int argc, char *argv[])
   if (argv[(argc - 1)] == "color")
   if (argc == 0)
     {
-      printf("Entering interactive mode...\n");
+      printf ("Entering interactive mode...\n");
       Py_Initialize ();
       
       pName = PyString_FromString ("initialize.py");
@@ -30,18 +29,6 @@ main (int argc, char *argv[])
 	  Py_DECREF (pModule);
 	  Py_Finalize ();
 	}
-      /*
-      printf ("Entering interactive mode... (type exit to exit)\n");
-      while (1)
-	{
-          printf ("color> ");
-          fgets (str, sizeof(str), stdin);
-            if (str == "exit")
-	      break;
-	    else
-	      colorize (str);
-	}
-      */
     }
   else
     colorize (argv[argc]);
