529e75fd8661fa5778af847a14999c5ca6d1d316
Austin Dunn <awd123ps@gmail.com>
2015-10-07 20:31:42 -0500

    Updated header.

--- src/color.h
+++ src/color.h
@@ -6,18 +6,18 @@ colorize (char string[])
 {
   int i, n;
 
-  for (i = 0, n = 0; i < strlen(string); i++, n++)
+  for (i = 0, n = 0; i < strlen (string); i++, n++)
     {
-      if (i == (strlen(string) - 1))
-        printf("\e[%d;3%dm%c\e[0m\n",
-	       ((n < 8) ? 0 : 1),
-	       ((n < 8) ? n : (n - 7)),
-	       string[i]);
+      if (i == (strlen (string) - 1))
+        printf ("\e[%d;3%dm%c\e[0m\n",
+	        ((n < 8) ? 0 : 1),
+	        ((n < 8) ? n : (n - 7)),
+	        string[i]);
       else
-        printf("\e[%d;3%dm%c",
-	       ((n < 8) ? 0 : 1),
-	       ((n < 8) ? n : (n - 7)),
-	       string[i]);
+        printf ("\e[%d;3%dm%c",
+	        ((n < 8) ? 0 : 1),
+	        ((n < 8) ? n : (n - 7)),
+	        string[i]);
 
       if (n == 14)
 	n -= 14;
