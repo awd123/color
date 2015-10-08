3dde06d882b18501c447970fa4287bf83986bcff
Austin Dunn <awd123ps@gmail.com>
2015-10-07 19:52:48 -0500

    Added Python.

--- src/color.c
+++ src/color.c
@@ -2,27 +2,49 @@
 #include "color.h"
 
 int
-main(int argc, char *argv[])
+main (int argc, char *argv[])
 {
+  PyObject *pName, *pModule, *pDict, *pFunc;
+  PyObject *pArgs, *pValue;
+  int i;
+  
   char str[1000];
+  
   str[1000] = '\0';
 
   if (argv[(argc - 1)] == "color")
   if (argc == 0)
     {
-      printf("Entering interactive mode... (type exit to exit)\n");
+      printf("Entering interactive mode...\n");
+      Py_Initialize ();
+      
+      pName = PyString_FromString ("initialize.py");
+      pModule = PyImport_Import (pName);
+      Py_DECREF (pName);
+
+      if (pModule != NULL)
+	{
+	  pFunc = PyObject_GetAttrString (pModule, "interactive");
+
+	  Py_XDECREF (pFunc);
+	  Py_DECREF (pModule);
+	  Py_Finalize ();
+	}
+      /*
+      printf ("Entering interactive mode... (type exit to exit)\n");
       while (1)
 	{
-          printf("color> ");
-          fgets(str, sizeof(str), stdin);
+          printf ("color> ");
+          fgets (str, sizeof(str), stdin);
             if (str == "exit")
 	      break;
 	    else
-	      colorize(str);
+	      colorize (str);
 	}
+      */
     }
   else
-    colorize(argv[argc]);
+    colorize (argv[argc]);
 
   return 0;
 }
