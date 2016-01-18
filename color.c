4536bb030c1c3ae9032ca73a8d4dfb30fbc68e59
Austin Dunn <awd123ps@gmail.com>
2016-01-17 19:41:12 -0600

    Clean ./configure and Makefile generation

---
+++ color.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  void colorize(char *string);
+
+  if (argc == 0)
+  {
+    char *str;
+    while (1)
+    {
+      fgets(str, 1024, stdin);
+      colorize(str);
+    }
+  }
+  else
+    colorize(argv[1]);
+
+  return 0;
+}
+
+void colorize(char *string) {
+  int i, n;
+
+  for (i = 0, n = 0; i < strlen(string); i++, n++)
+  {
+    if (i == (strlen(string) - 1))
+      printf("\e[%d;3%dm%c\e[0m\n",
+              ((n < 8) ? 0 : 1),
+              ((n < 8) ? n : (n - 7)),
+              string[i]);
+    else
+      printf("\e[%d;3%dm%c",
+              ((n < 8) ? 0 : 1),
+              ((n < 8) ? n : (n - 7)),
+              string[i]);
+
+    if (n == 14)
+      n -= 14;
+  }
+}
